b7b4144b98f2f8161bb491f1d26e46b4447d8822
JP <johnpaul.soriano.6197@gmail.com>
2021-04-15 14:56:33 +0800

    041521

--- interface_config.js
+++ interface_config.js
@@ -253,12 +253,12 @@ var interfaceConfig = {
     /**
      * Specify mobile app scheme for opening the app from the mobile browser.
      */
-    // APP_SCHEME: 'org.jitsi.meet',
+    APP_SCHEME: 'com.arzatech.owline',
 
     /**
      * Specify the Android app package name.
      */
-    // ANDROID_APP_PACKAGE: 'org.jitsi.meet',
+    ANDROID_APP_PACKAGE: 'com.arzatech.owline',
 
     /**
      * Override the behavior of some notifications to remain displayed until
